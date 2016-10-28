be0d18129dd4f2fedaffd6dd21471ba6967aaf26
Mihai <Mihai@DESKTOP-L121NP4>
2016-10-28 12:14:41 +0300

    +dd

--- CM-App/cm/start/StartApp.java
+++ CM-App/cm/start/StartApp.java
@@ -2,7 +2,14 @@ package cm.start;
 
 public class StartApp {
 
-	public static void main(String[] args) {
+	private String message;
 
+	public void setMessage(String message) {
+		this.message = message;
 	}
+
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+
 }
